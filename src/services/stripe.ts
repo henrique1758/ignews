31770d845c69538611d7dd48b91b00e737ef32b1
Henrique Monteiro <henriquemonteiro942@gmail.com>
2022-04-24 14:05:34 -0300

    project completed

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,7 +1,7 @@
 import { Stripe } from "stripe";
 import { version } from "../../package.json";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY, {
+export const stripe = new Stripe(String(process.env.STRIPE_SECRET_API_KEY), {
   apiVersion: '2020-08-27',
   appInfo: {
     name: 'Ignews',
